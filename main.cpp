449cd08506bf5f16118abdcc060291885720244b
NeptuneLuke <luca1cortinovis@gmail.com>
2025-04-28 23:27:26 +0200

    Added GLFW basic window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>	// include GLFW definitions and load the Vulkan header
 
 #include <iostream>		// reporting errors
 #include <stdexcept>	// reporting errors
 #include <cstdlib>		// miscellaneous utilities (EXIT_SUCCESS, EXIT_FAILURE)
+
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 /*
  * This class stores the Vulkan objects
@@ -20,6 +25,8 @@ public:
 		// which will propagate back to the main function and catched
 		// by the general std::exception
 
+		init_window();
+
 		init_vulkan();
 
 		main_loop();
@@ -30,6 +37,19 @@ public:
 
 private:
 
+	GLFWwindow* window;
+
+	// Initialize a GLFW window
+	void init_window() {
+
+		glfwInit();    // Initialize GLFW library
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Do not create an OpenGL context -> GLFW_NO_API
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);	  // For now, disable window resizing
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	// Initialize the Vulkan objects
 	void init_vulkan() {
 
@@ -38,11 +58,20 @@ private:
 	// Iterates render operations until the window is closed
 	void main_loop() {
 
+		// Keep running until an error occurs or the window is closed
+		while(!glfwWindowShouldClose(window)) {
+
+			glfwPollEvents();
+		}
+
 	}
 
 	// Deallocate resources
 	void cleanup() {
 
+		glfwDestroyWindow(window);
+
+		glfwTerminate(); // Shutdown GLFW library
 	}
 
 };
